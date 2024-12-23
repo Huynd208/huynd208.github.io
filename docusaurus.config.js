24dfdb272feb4422aa95ce18c404c6e41e1b1c70
Huynd <huynd4k@gmail.com>
2024-12-23 21:45:10 +0700

    docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Huynd',
+  title: 'Đức Huy',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
